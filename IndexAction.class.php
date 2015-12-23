d23dbc013e968420d09c2055c91ffe7ba1ecd5c7
gator <791264638@qq.com>
2015-12-23 17:40:17 +0800

    对添加的信息进行补充

--- IndexAction.class.php
+++ IndexAction.class.php
@@ -1,10 +1,14 @@
 <?php
+/*
+*微信消息推送
+*根据具体需求,做相应的调用处理
+*/
 class IndexAction extends CommentAction {
     /*微信消息推送开始*/
     public function sendwx(){
-        $appid='wx313dff800dfbe6d3';//小伙子自己变为动态的吧
-        $appsecret='ba53e5ea64e2dcb0efd0169af143b4fc';//同上     
-        $touser='o1kcqxFwd2177UPu_Jbo_8lt4WO0';//这就是openid(小伙子自己改吧)
+        $appid='wx313dff800dfbe6d3';//公众号appid
+        $appsecret='ba53e5ea64e2dcb0efd0169af143b4fc';//公众号秘钥     
+        $touser='o1kcqxFwd2177UPu_Jbo_8lt4WO0';//openid
         $template_id='I2WAYWqZE-V9hHDa3nTsJEjBDnIl5SN-PAP4mJVfs7k';//这是消息模板ID(可以更改,就是选择不同的消息推送类型)
         $url='http://www.baidu.com';//随便定义,看着办吧
         //数据样例(根据不同的消息模板组装不同的消息格式(具体查看公众号消息模板))
